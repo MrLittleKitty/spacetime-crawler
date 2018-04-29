b7dde6e86928da62e64f83fa93dad644f160614b
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2018-04-28 22:04:57 -0700

    More filtering of crawler traps

--- applications/search/crawler_frame.py
+++ applications/search/crawler_frame.py
@@ -1,5 +1,8 @@
 import logging
 import sys
+
+from collections import defaultdict
+
 from datamodel.search.EawolfeSantiadmTevinl1_datamodel import EawolfeSantiadmTevinl1Link, \
     OneEawolfeSantiadmTevinl1UnProcessedLink
 from spacetime.client.IApplication import IApplication
@@ -60,6 +63,11 @@ class CrawlerFrame(IApplication):
 URL_REGEX = re.compile(
     ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
 
+domainMap = defaultdict(lambda: 0)
+
+mostLinks = 0
+page = ""
+
 
 def extract_next_links(rawDataObj):
     outputLinks = []
@@ -70,14 +78,17 @@ def extract_next_links(rawDataObj):
     Validation of link via is_valid function is done later (see line 42).
     It is not required to remove duplicates that have already been downloaded. 
     The frontier takes care of that.
-    
-    Suggested library: lxml
     '''
+
     url = None
     if rawDataObj.is_redirected:
         url = rawDataObj.final_url
     else:
         url = rawDataObj.url
+
+    parsed = urlparse(url)
+    domain = parsed.hostname[0:parsed.hostname.find('.ics.uci.edu')]
+    domainMap[domain] = domainMap[domain] + 1
 
     if rawDataObj.content is not None and rawDataObj.content is not "":
         try:
@@ -86,23 +97,33 @@ def extract_next_links(rawDataObj):
             doc.make_links_absolute(url)
             for href in doc.iterlinks():
                 if URL_REGEX.match(href[2]):  # Make sure that any link is an absolute address
-                    outputLinks.append(processLink(href[2]))
+                    outputLinks.append(processLink(href[2], parsed))
         except:
             # Treat the raw content as a plain text and search for urls in it
             links = re.findall(URL_REGEX, rawDataObj.content)
             for link in links:
-                outputLinks.append(processLink(link))
+                outputLinks.append(processLink(link, parsed))
+
+    global mostLinks
+    if len(outputLinks) > mostLinks:
+        mostLinks = len(outputLinks)
+        global page
+        page = url
 
     return outputLinks
 
 
 AFG_PAGE = re.compile(r'afg[0-9]+_page_id')
+REPLY_TO = re.compile(r'replytocom=[0-9]+')
 
 
-def processLink(url):
+def processLink(url, parsed):
     # Remove all the query parameters for links from wics that have the AFG page parameter
     if AFG_PAGE.search(url):
         return url[0:url.find('?')]
+
+    if 'replytocom=' in parsed.query:
+        return re.sub(REPLY_TO, '', url)
 
     return url
 
@@ -123,11 +144,14 @@ def is_valid(url):
                             + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
                             + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
                             + "|thmx|mso|arff|rtf|jar|csv" \
-                            + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower()):
+                            + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf|ova)$", parsed.path.lower()):
             return False
 
         # Stop crawler trap on the WICS site with the afg_page_id query parameter
         if AFG_PAGE.search(parsed.query):
+            return False
+
+        if 'replytocom=' in parsed.query:
             return False
 
         # Word-press JSON apis are a black hole, filter them out
